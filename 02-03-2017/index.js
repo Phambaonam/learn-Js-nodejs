a19f460f1bdfaf081e8922f46f74575e40bee041
Phambaonam <phambaonam1312@gmail.com>
2017-03-25 02:05:29 +0700

    test

---
+++ 02-03-2017/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by phambaonam on 02/03/2017.
+ */
+let arr = [
+    'doremon [1-50]',
+    'doremon [200-300]',
+    'doremon [100-199]',
+    'doremon [51-99]'
+];
+
+// TODO: Hãy sắp xêp lại mảng trên theo đúng thứ tự tăng dần
+
+console.log(arr.sort());
+
+
